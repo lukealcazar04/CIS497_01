95332499f28bdde44107066039be5f37edd0a9d2
lukealcazar04 <lukealcazar04@users.noreply.github.com>
2020-09-06 23:37:12 -0500

    Challenge1

---
+++ Challenge 1/Assets/Challenge 1/Scripts/TriggerZoneAddScoreOnce.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * (Luke Alcazar)
+ * (Challenge 1)
+ * (This uses the trigger zones to add to the score)
+ */
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerZoneAddScoreOnce : MonoBehaviour
+{
+    private bool triggered = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !triggered)
+        {
+            triggered = true;
+            ScoreManager.score++;
+        }
+    }
+
+}
